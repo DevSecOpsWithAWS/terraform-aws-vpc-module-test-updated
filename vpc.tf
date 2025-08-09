f07293763edd8af69b5df89aeb6b69f88cb01970
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-08-09 08:16:36 +0530

    vpc

--- vpc.tf
+++ vpc.tf
@@ -5,4 +5,9 @@ module "vpc" {
   cidr_block = var.cidr_block
   common_tags = var.common_tags
   vpc_tags = var.vpc_tags
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
+  is_peering_required = true
 }
+
